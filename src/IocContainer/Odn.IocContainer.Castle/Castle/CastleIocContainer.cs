5e16d21a0600e91f2ab4c881c6b6dbbeab259162
DukeCheng <flyear@flyear.net>
2016-03-16 22:05:20 +0800

    Added CastleIocContainer Project

---
+++ src/IocContainer/Odn.IocContainer.Castle/Castle/CastleIocContainer.cs
@@ -0,0 +1,13 @@
+﻿using Odn.DependencyContainer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odn.IocContainer.Castle
+{
+    public class CastleIocContainer : IIocContainer
+    {
+    }
+}
