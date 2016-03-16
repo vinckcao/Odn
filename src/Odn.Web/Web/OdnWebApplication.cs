b6d1ef3badefe6887d43013fe3093ddb02ae90f4
DukeCheng <flyear@flyear.net>
2016-03-16 22:05:56 +0800

    Added Odn.Web Module

---
+++ src/Odn.Web/Web/OdnWebApplication.cs
@@ -0,0 +1,73 @@
+﻿using Odn.Dependency.Extensions;
+using Odn.Reflection;
+using System;
+using System.Web;
+
+namespace Odn.Web
+{
+    public class OdnWebApplication : HttpApplication
+    {
+        /// <summary>
+        /// Gets a reference to the <see cref="AbpBootstrapper"/> instance.
+        /// </summary>
+        protected OdnBootstrapper OdnBootstrapper { get; private set; }
+
+        protected OdnWebApplication()
+        {
+            OdnBootstrapper = new OdnBootstrapper();
+        }
+
+        /// <summary>
+        /// This method is called by ASP.NET system on web application's startup.
+        /// </summary>
+        protected virtual void Application_Start(object sender, EventArgs e)
+        {
+            OdnBootstrapper.IocManager.RegisterIfNot<IAssemblyFinder, WebAssemblyFinder>();
+            OdnBootstrapper.Initialize();
+        }
+
+        protected virtual void Application_End(object sender, EventArgs e)
+        {
+            OdnBootstrapper.Dispose();
+        }
+
+        protected virtual void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected virtual void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// This method is called by ASP.NET system when a request starts.
+        /// </summary>
+        protected virtual void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //var langCookie = Request.Cookies["Abp.Localization.CultureName"];
+            //if (langCookie != null && GlobalizationHelper.IsValidCultureCode(langCookie.Value))
+            //{
+            //    Thread.CurrentThread.CurrentCulture = new CultureInfo(langCookie.Value);
+            //    Thread.CurrentThread.CurrentUICulture = new CultureInfo(langCookie.Value);
+            //}
+        }
+
+        /// <summary>
+        /// This method is called by ASP.NET system when a request ends.
+        /// </summary>
+        protected virtual void Application_EndRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected virtual void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+        }
+
+        protected virtual void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ src/Odn.Web/Web/WebAssemblyFinder.cs
@@ -0,0 +1,47 @@
+﻿using Odn.Reflection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Compilation;
+
+namespace Odn.Web
+{
+    /// <summary>
+    /// This class is used to get all assemblies in bin folder of a web application.
+    /// </summary>
+    public class WebAssemblyFinder : IAssemblyFinder
+    {
+        /// <summary>
+        /// The search option used to find assemblies in bin folder.
+        /// </summary>
+        public static SearchOption FindAssembliesSearchOption = SearchOption.TopDirectoryOnly;
+
+        /// <summary>
+        /// This return all assemblies in bin folder of the web application.
+        /// </summary>
+        /// <returns>List of assemblies</returns>
+        public List<Assembly> GetAllAssemblies()
+        {
+            var assembliesInBinFolder = new List<Assembly>();
+
+            var allReferencedAssemblies = BuildManager.GetReferencedAssemblies().Cast<Assembly>().ToList();
+            var dllFiles = Directory.GetFiles(HttpRuntime.AppDomainAppPath + "bin\\", "*.dll", FindAssembliesSearchOption).ToList();
+
+            foreach (string dllFile in dllFiles)
+            {
+                var locatedAssembly = allReferencedAssemblies.FirstOrDefault(asm => AssemblyName.ReferenceMatchesDefinition(asm.GetName(), AssemblyName.GetAssemblyName(dllFile)));
+                if (locatedAssembly != null)
+                {
+                    assembliesInBinFolder.Add(locatedAssembly);
+                }
+            }
+
+            return assembliesInBinFolder;
+        }
+    }
+}
