133dc61b179930330b7159a387ca0bf102818a94
DukeCheng <dukecheng@yaomaiche.com>
2016-04-22 01:11:07 +0800

    added test project

---
+++ src/Odn.Tests/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Odn.Dependency;
+using Xunit;
+
+namespace Odn.Tests
+{
+    public abstract class TestBaseWithLocalIocManager : IDisposable
+    {
+        protected IIocManager LocalIocManager;
+
+        protected TestBaseWithLocalIocManager()
+        {
+            LocalIocManager = new IocManager();
+        }
+
+        public virtual void Dispose()
+        {
+            LocalIocManager.Dispose();
+        }
+    }
+
+    public class IocManager_Tests : TestBaseWithLocalIocManager
+    {
+        [Fact]
+        public void Should_Get_First_Registered_Class_If_Registered_Multiple_Class_For_Same_Interface()
+        {
+            LocalIocManager.Register<IEmpty, EmptyImplOne>();
+            LocalIocManager.Register<IEmpty, EmptyImplTwo>(); //Second registered has no effect!
+            var obj = LocalIocManager.Resolve<IEmpty>();
+            Assert.True(obj is EmptyImplOne);
+
+            //obj.GetType().ShouldBe(typeof(EmptyImplOne));
+
+            //LocalIocManager.Resolve<IEmpty>().GetType().ShouldBe(typeof(EmptyImplOne));
+        }
+
+        public interface IEmpty
+        {
+
+        }
+
+        public class EmptyImplOne : IEmpty
+        {
+
+        }
+
+        public class EmptyImplTwo : IEmpty
+        {
+
+        }
+    }
+}
---
+++ src/Odn/Dependency/IocManager.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Autofac;
+using Autofac.Core.Lifetime;
+
+namespace Odn.Dependency
+{
+    public class IocManager : IIocManager, IIocRegistrar, IIocResolver, IDisposable
+    {
+        private readonly IContainer _container;
+
+        public virtual IContainer Container
+        {
+            get
+            {
+                return _container;
+            }
+        }
+
+        public IocManager(IContainer container)
+        {
+            this._container = container;
+
+            //_container.ComponentRegistry.Register();
+            //IocContainer = new WindsorContainer();
+            //_conventionalRegistrars = new List<IConventionalDependencyRegistrar>();
+
+            ////Register self!
+            //IocContainer.Register(
+            //    Component.For<IocManager, IIocManager, IIocRegistrar, IIocResolver>().UsingFactoryMethod(() => this)
+            //    );
+        }
+
+        public void RegisterAssemblyByConvention(Assembly assembly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<T>(DependencyLifeStyle lifeStyle = DependencyLifeStyle.Singleton) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type type, DependencyLifeStyle lifeStyle = DependencyLifeStyle.Singleton)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TType, TImpl>(DependencyLifeStyle lifeStyle = DependencyLifeStyle.Singleton) where TType : class where TImpl : class, TType
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type type, Type impl, DependencyLifeStyle lifeStyle = DependencyLifeStyle.Singleton)
+        {
+            
+        }
+
+        public bool IsRegistered(Type type)
+        {
+            return _container.IsRegistered(type);
+        }
+
+        public bool IsRegistered<T>()
+        {
+            return _container.IsRegistered<T>();
+        }
+
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        public T Resolve<T>(object argumentsAsAnonymousType)
+        {
+            //return _container.Resolve<T>()
+            throw new NotSupportedException();
+        }
+
+        public object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+
+        public object Resolve(Type type, object argumentsAsAnonymousType)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Resolve
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">key</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual T Resolve<T>(string key = "", ILifetimeScope scope = null) where T : class
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            if (string.IsNullOrEmpty(key))
+            {
+                return scope.Resolve<T>();
+            }
+            return scope.ResolveKeyed<T>(key);
+        }
+
+        /// <summary>
+        /// Resolve
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object Resolve(Type type, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            return scope.Resolve(type);
+        }
+
+        /// <summary>
+        /// Resolve all
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">key</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved services</returns>
+        public virtual T[] ResolveAll<T>(string key = "", ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            if (string.IsNullOrEmpty(key))
+            {
+                return scope.Resolve<IEnumerable<T>>().ToArray();
+            }
+            return scope.ResolveKeyed<IEnumerable<T>>(key).ToArray();
+        }
+
+        /// <summary>
+        /// Resolve unregistered service
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual T ResolveUnregistered<T>(ILifetimeScope scope = null) where T : class
+        {
+            return ResolveUnregistered(typeof(T), scope) as T;
+        }
+
+        /// <summary>
+        /// Resolve unregistered service
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object ResolveUnregistered(Type type, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            var constructors = type.GetConstructors();
+            foreach (var constructor in constructors)
+            {
+                try
+                {
+                    var parameters = constructor.GetParameters();
+                    var parameterInstances = new List<object>();
+                    foreach (var parameter in parameters)
+                    {
+                        var service = Resolve(parameter.ParameterType, scope);
+                        if (service == null) throw new Exception("Unkown dependency");
+                        parameterInstances.Add(service);
+                    }
+                    return Activator.CreateInstance(type, parameterInstances.ToArray());
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            throw new Exception("No constructor  was found that had all the dependencies satisfied.");
+        }
+
+        /// <summary>
+        /// Try to resolve srevice
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <param name="instance">Resolved service</param>
+        /// <returns>Value indicating whether service has been successfully resolved</returns>
+        public virtual bool TryResolve(Type serviceType, ILifetimeScope scope, out object instance)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            return scope.TryResolve(serviceType, out instance);
+        }
+
+        /// <summary>
+        /// Check whether some service is registered (can be resolved)
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Result</returns>
+        public virtual bool IsRegistered(Type serviceType, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            return scope.IsRegistered(serviceType);
+        }
+
+        /// <summary>
+        /// Resolve optional
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object ResolveOptional(Type serviceType, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            return scope.ResolveOptional(serviceType);
+        }
+
+        public void Release(object obj)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        /// <summary>
+        /// Get current scope
+        /// </summary>
+        /// <returns>Scope</returns>
+        public virtual ILifetimeScope Scope()
+        {
+            try
+            {
+                //if (HttpContext.Current != null)
+                //    return AutofacDependencyResolver.Current.RequestLifetimeScope;
+
+                //when such lifetime scope is returned, you should be sure that it'll be disposed once used (e.g. in schedule tasks)
+                return Container.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
+            }
+            catch (Exception)
+            {
+                //we can get an exception here if RequestLifetimeScope is already disposed
+                //for example, requested in or after "Application_EndRequest" handler
+                //but note that usually it should never happen
+
+                //when such lifetime scope is returned, you should be sure that it'll be disposed once used (e.g. in schedule tasks)
+                return Container.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
+            }
+        }
+    }
+}
